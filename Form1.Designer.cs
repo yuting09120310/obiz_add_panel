6d43d14028cbc7420268e4450c44c6f77440bc44
alex <alex@PC-ALEX-obiz>
2022-03-23 18:19:21 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace obiz_add_panel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.panel2.Location = new System.Drawing.Point(12, 52);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(712, 467);
+            this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(746, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(746, 137);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 525);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace obiz_add_panel
+{
+    public partial class Form1 : Form
+    {
+        public int i = 0;
+
+        public Create[] ct = new Create[5];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //按下新增後 調用create 生成控制項 並將生成出來的物件 綁定事件
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ct[i] = new Create(panel2);
+            ct[i].Btn_send.Click += new EventHandler(Btn_send_Click);
+            ReName();
+        }
+
+
+        //動態生成的送出按鈕
+        protected void Btn_send_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(ct[i].text.Text);
+        }
+
+
+        //將panel取名 便於排版
+        public void ReName()
+        {
+            
+            foreach (Control control in panel2.Controls)
+            {
+                if(!control.Name.Contains("Panel"))
+                {
+                    control.Name = "Panel" + i;
+                    i++;
+                }
+            }
+            TypeSetting();
+        }
+
+        //將panel重新排版
+        public void TypeSetting()
+        {
+            foreach (Control control in panel2.Controls)
+
+                for (int j = 1; j < panel2.Controls.Count; j++)
+                {
+                    if (control.Name == $"Panel{j}")
+                    {
+                        control.Top = j * 100;
+                    }
+                }
+            }
+        }
+
+        //按下新增後 會創建新的panel以及子層物件
+        public class Create
+        {
+            public Button Btn_send = new Button()
+            {
+                Text = "123"
+            };
+
+            public TextBox text = new TextBox()
+            {
+                Text = "445566",
+                Width = 150
+            };
+
+            public Panel panel = new Panel()
+            {
+                Width = 712,
+                Height = 50,
+                BackColor = Color.Tan,
+            };
+
+            public Create(Panel panel2)
+            {
+                panel2.Controls.Add(panel);
+                panel.Controls.Add(Btn_send);
+                panel.Controls.Add(text);
+
+                Btn_send.Left = 400;
+                Btn_send.Top = 12;
+
+                text.Top = 12;
+                text.Left = 200;
+            }
+        }
+
+    }
+
